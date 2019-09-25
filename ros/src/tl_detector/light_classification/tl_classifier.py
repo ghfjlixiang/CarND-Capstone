d78c843cdf53b41c90dd92287d80f75ae50a80a9
ghfjlixiang <ghfjlixiang@126.com>
2019-09-25 23:17:31 +0800

    load frozen tensorflow model

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -7,7 +7,16 @@ from styx_msgs.msg import TrafficLight
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        #pass
+		# load frozen tensorflow model into memory
+		PATH_TO_FROZEN_GRAPH = 'light_classification/model/frozen_inference_graph_sim.pb'
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
