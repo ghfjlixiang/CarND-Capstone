ff3b30354bd2b75a26400be6e8bfb3d5d901a38d
ghfjlixiang <[A[Bghfjlixiang@126.com>
2019-10-07 23:59:53 +0800

    feat:Determine the traffic light color according to the average pixel value of HSV channel

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -76,3 +76,47 @@ class TLClassifier(object):
         output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
         output_dict['detection_scores'] = output_dict['detection_scores'][0]
         return output_dict
+
+    def light_color(self,lightImg):
+        """.
+
+        Args:
+            lightImg：
+
+        Returns:
+        """        
+        if self.image_result_show:
+            cv.namedWindow('light_image', cv.WINDOW_NORMAL)
+            cv.imshow('light_image', lightImg)
+            cv.waitKey(1)
+
+        lightImg = cv.cvtColor(lightImg, cv.COLOR_BGR2HSV)    # translate bgr to HSV
+        
+        height,width,depth = lightImg.shape 
+    
+        height_start = int (height/8)      # split 3 part in height 
+        height_1st = int(height/3)        
+        height_2nd = int(height*2/3)      
+        height_end = int(height*7/8)
+
+        lightImg_red = lightImg[height_start:height_1st,:,2]     # red part image
+        lightImg_yellow = lightImg[height_1st:height_2nd,:,2]    # yellow part
+        lightImg_green = lightImg[height_2nd:height_end,:,2]     # green part
+
+        # Calculating the average of pixels each color part image
+        red_height,red_width = lightImg_red.shape
+        red_pixel_sum = np.sum(lightImg_red)/(red_height*red_width)
+
+        yellow_height,yellow_width = lightImg_yellow.shape
+        yellow_pixel_sum = np.sum(lightImg_yellow)/(yellow_height*yellow_width)
+
+        green_height,green_width = lightImg_green.shape
+        green_pixel_sum = np.sum(lightImg_green)/(green_height*green_width)
+
+        print ("red is%f, yellow is %f, green is %f"%(red_pixel_sum,yellow_pixel_sum,green_pixel_sum))
+        # red part is biggest indicate red light
+        if red_pixel_sum > yellow_pixel_sum and red_pixel_sum > green_pixel_sum:
+            current_light = TrafficLight.RED
+        else:
+            current_light = TrafficLight.GREEN
+        return current_light
